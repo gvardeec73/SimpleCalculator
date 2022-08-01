9f0cab3f3658f02f42d5b903c15892ad13ce0a7f
Valeriy Shunin <sadgvardeec@yandex.ru>
2022-08-01 10:27:51 +0400

    Simple calculator reads from and writes to File

--- src/Main.java
+++ src/Main.java
@@ -16,26 +16,32 @@ public class Main {
         File inputFile = new File(readPath);
         File outputFile = new File(writePath);
         Scanner sc = new Scanner(inputFile);
-        String str = sc.nextLine();
-        sc.close();
-        if (!outputFile.exists()) outputFile.createNewFile();
-        else System.out.println("File exists");
-        PrintWriter pw = new PrintWriter(outputFile);
+        ArrayList <String> strList = new ArrayList<>();
+        while (sc.hasNext()) {
+            strList.add(sc.nextLine());
 
-
-        String [] symbols = str.split(" ");
-        double a = 0.0;
-        double b = 0.0;
-        boolean wasException = false;
-
-        try {
-            a = Double.valueOf(symbols[0]);
-            b = Double.valueOf(symbols[2]);
-        } catch (NumberFormatException e) {
-            System.out.println("Error! Not number");
-            pw.println("Error! Not number");
-            wasException = true;
         }
+        sc.close();
+
+
+       PrintWriter pw = new PrintWriter(outputFile);
+
+        for (int i = 0; i < strList.size(); i++) {
+
+
+            String[] symbols = strList.get(i).split(" ");
+            double a = 0.0;
+            double b = 0.0;
+            boolean wasException = false;
+
+            try {
+                a = Double.valueOf(symbols[0]);
+                b = Double.valueOf(symbols[2]);
+            } catch (NumberFormatException e) {
+                System.out.println(strList.get(i) + " " + "=" + " Error! Not number");
+                pw.println(strList.get(i) + " " + "=" + " Error! Not number");
+                wasException = true;
+            }
 
 
             ArrayList<Character> arithmeticOperations = new ArrayList<>();
@@ -44,59 +50,59 @@ public class Main {
             arithmeticOperations.add('*');
             arithmeticOperations.add('/');
             char[] operation = symbols[1].toCharArray();
-        if (!wasException) {
-            try {
+            if (!wasException) {
+                try {
 
-                if (operation.length != 1) {
-                    throw new ArithmeticException("Operation Error!");
-                } else if (!arithmeticOperations.contains(operation[0])) {
-                    throw new ArithmeticException("Operation Error!");
+                    if (operation.length != 1) {
+                        throw new ArithmeticException("Operation Error!");
+                    } else if (!arithmeticOperations.contains(operation[0])) {
+                        throw new ArithmeticException("Operation Error!");
+                    }
+
+                } catch (ArithmeticException e) {
+                    System.out.println(strList.get(i) + " " + "=" + " Operation Error!");
+                    pw.println(strList.get(i) + " " + "=" + " Operation Error!");
+                    wasException = true;
                 }
+            }
+            if (!wasException) {
+                try {
+                    if (operation[0] == '/' && b == 0) {
+                        throw new ArithmeticException("Error! Division by zero");
+                    }
+                } catch (ArithmeticException e) {
+                    System.out.println(strList.get(i) + " " + "=" + " Error! Division by zero");
+                    pw.println(strList.get(i) + " " + "=" + " Error! Division by zero");
+                    wasException = true;
+                }
+            }
 
-            } catch (ArithmeticException e) {
-                System.out.println("Operation Error!");
-                pw.println("Operation Error!");
-                wasException = true;
+            if (!wasException) {
+                double result = 0.0;
+                switch (operation[0]) {
+                    case '+': {
+                        result = a + b;
+                        break;
+                    }
+                    case '-': {
+                        result = a - b;
+                        break;
+                    }
+                    case '*': {
+                        result = a * b;
+                        break;
+                    }
+                    case '/': {
+                        result = a / b;
+                        break;
+                    }
+                }
+                System.out.println(strList.get(i) + " " + "= " + result);
+                pw.println(strList.get(i) + " " + "= " + result);
+
+
             }
         }
-        if (!wasException) {
-            try {
-                if (operation[0] == '/' && b == 0) {
-                    throw new ArithmeticException("Error! Division by zero");
-                }
-            } catch (ArithmeticException e) {
-                System.out.println("Error! Division by zero");
-                pw.println("Error! Division by zero");
-                wasException = true;
-            }
-        }
-
-       if (!wasException) {
-           double result = 0.0;
-           switch (operation[0]) {
-               case '+': {
-                   result = a + b;
-                   break;
-               }
-               case '-': {
-                   result = a - b;
-                   break;
-               }
-               case '*': {
-                   result = a * b;
-                   break;
-               }
-               case '/': {
-                   result = a / b;
-                   break;
-               }
-           }
-           System.out.println(result);
-           pw.println(result);
-
-
-
-       }
         pw.flush();
         pw.close();
     }
